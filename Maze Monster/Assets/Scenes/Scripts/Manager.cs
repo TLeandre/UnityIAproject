945c16dbb584ab96743825c64483974ec5023c32
LeandreDevelopper <59528877+LeandreDevelopper@users.noreply.github.com>
2021-04-05 03:05:24 +0200

    add maze IA

---
+++ Maze Monster/Assets/Scenes/Scripts/Manager.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour {
+
+	public Text NBG;
+	public Text fitMoyen;
+	public Text Population;
+	public Text bestFit;
+	public Text badFit;
+
+	public GameObject catPrefab;			//Prefab de notre chat
+
+	private bool isTraning = false;
+	public int populationSize = 50;			//Nombre d'agent a faire spawner. Gardez un multiple de 4 svp
+	public float timer = 10f;				//Temps par generation
+	private int generationNumber = 0;		//Numero de la generation actuelle
+
+	private List<NeuralNetwork> nets;		//Liste de neural networks de generation x
+	private List<NeuralNetwork> newNets;	//Liste de neural networks de generation x+1
+	public List<GameObject> catList = null;	//Liste des chats de notre generation
+
+
+	private int[] layers = new int[] { 6, 8, 6, 2 }; //Dimension de nos reseaux de neurones
+
+	private float fit=0;					//On calculera la moyenne de fitnes de la generation grace a cette variable
+
+	void Start()
+	{
+			NBG.text = "Nombre de Generation : " + generationNumber;
+			fitMoyen.text = "Fitness Moyen : " + fit;
+			Population.text = "Population : " + populationSize;
+			bestFit.text = "Meilleur Fitness : 0";
+			badFit.text = "Pire Fitness : 0";
+
+	}
+
+	// Met fin a la generation actuelle (cheh)
+	void Timer()
+	{
+		isTraning = false;
+	}
+
+	void Update ()
+	{
+		//Changement de generation
+		if (isTraning == false)
+		{
+			// Si c'est la premiere generation, instancie les tichats
+			if (generationNumber == 0)
+			{
+				InitCatNeuralNetworks();
+				CreateCatBodies();
+			}
+			else
+			{
+				//Transfer le score de fitness du controleur vers le reseau de neurones
+				for(int i=0; i<populationSize; i++)
+				{
+					NNController script = catList[i].GetComponent<NNController>();
+					float fitness = script.fitness;
+					nets[i].SetFitness(fitness);
+				}
+
+				//Trie les agents pour ne garder que les plus performants
+				nets.Sort();
+				nets.Reverse();
+
+				//Affiche la moyenne de fitness de la generation
+				fit = 0;
+				for(int i=0; i<populationSize; i++){
+					fit += nets[i].GetFitness();
+				}
+				fit/=populationSize;
+				Debug.Log("Average fitness:");
+				Debug.Log(fit);
+
+				majInformations();
+
+				//Instancie la liste de la generation suivante
+				List<NeuralNetwork> newNets = new List<NeuralNetwork>();
+
+				//Recupere les plus intelligent de nos tichats
+				for (int i = 0; i < populationSize/4; i++)
+				{
+					NeuralNetwork net = new NeuralNetwork(nets[i]);
+					newNets.Add (net);
+
+				}
+
+				//Recupere les plus intelligent de nos tichats et les fait  muter
+				for (int i = 0; i < populationSize/4; i++)
+				{
+					NeuralNetwork net = new NeuralNetwork(nets[i]);
+					net.Mutate(0.5f);
+					newNets.Add (net);
+				}
+
+				//Recupere les plus intelligent de nos tichatset les fait plus muter
+				for (int i = 0; i < populationSize/4; i++)
+				{
+					NeuralNetwork net = new NeuralNetwork(nets[i]);
+					net.Mutate(2f);
+					newNets.Add (net);
+
+				}
+
+				//Recupere les plus intelligent de nos tichats et leurs defonce le cerveau
+				for (int i = 0; i < populationSize/4; i++)
+				{
+					NeuralNetwork net = new NeuralNetwork(nets[i]);
+					net.Mutate(10f);
+					newNets.Add (net);
+				}
+
+				//Changement d'agents entre les deux generation
+				nets = newNets;
+
+			}
+
+			//A la fin du decompte du timer, passe a la generation suivante
+			generationNumber++;
+			Invoke("Timer",timer);
+			CreateCatBodies();
+
+
+			isTraning = true;
+		}
+
+		//------------------FEEDFORWARD
+		//Transfer les informations du NNController vers l'input layer du reseau de neurones
+		for (int i = 0; i < populationSize; i++)
+		{
+			NNController script = catList[i].GetComponent<NNController>();
+
+			float[] result;
+			float vel = script.currentVelocity;
+			float distForward = script.distForward/script.maxDistance;
+			float distLeft= script.distLeft/script.maxDistance;
+			float distRight= script.distRight/script.maxDistance;
+			float distDiagLeft= script.distDiagLeft/script.maxDistance;
+			float distDiagRight= script.distDiagRight/script.maxDistance;
+
+			float[] tInputs = new float[]{vel, distForward, distLeft, distRight, distDiagLeft, distDiagRight};
+			result = nets[i].FeedForward(tInputs);
+			script.results = result;//Envoie le resultat au controleur
+
+		}
+
+        //Les generation ne meurent pas assez vite pour vous? Voila la solution
+		if (Input.GetKeyDown("space"))
+		{
+			generationNumber++;
+			isTraning = true;
+			Timer ();
+			CreateCatBodies();
+
+		}
+
+	}
+
+	//Instancie tout nos tichats
+	private void CreateCatBodies()
+	{
+		//Detruit tout nos tichats (muahaha)
+		for (int i = 0; i < catList.Count; i++)
+		{
+			Destroy(catList[i]);
+		}
+
+		//Recree une generation de tichats (mooooooh)
+		catList = new List<GameObject>();
+		for (int i = 0; i < populationSize; i++)
+		{
+			GameObject cat = Instantiate(catPrefab, new Vector3(12.5f, 0.1f, -10.5f),Quaternion.Euler(0, Random.Range(0, 380), 0));
+			catList.Add(cat);
+			catList[i] = cat;
+		}
+	}
+
+	// Initialise notre liste d'agent
+	void InitCatNeuralNetworks()
+	{
+		nets = new List<NeuralNetwork>();
+
+		for (int i = 0; i < populationSize; i++)
+		{
+			NeuralNetwork net = new NeuralNetwork(layers);
+			net.Mutate(0.5f);
+			nets.Add(net);
+		}
+	}
+
+	void majInformations()
+	{
+			NBG.text = "Nombre de Generation : " + generationNumber;
+			fitMoyen.text = "Fitness Moyen : " + fit;
+			bestFit.text = "Meilleur Fitness : " + nets[0].GetFitness();
+			badFit.text = "Pire Fitness : " + nets[39].GetFitness();
+	}
+
+
+}
---
+++ Maze Monster/Assets/Scenes/Scripts/NNController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NNController : MonoBehaviour {
+
+
+	private Transform transf;
+
+	private Vector3 movDir = Vector3.zero;	//Vecteur de deplacement
+	private float rotSpeed = 2f;        	//Vitesse de rotation
+	private float speed = 0.8f;           	//Facteur de vitesse
+	private float initialVelocity = 0.0f;	//Vitesse initiale
+	private float finalVelocity  = 2f;		//Vitesse maximale
+	public float currentVelocity  = 0.0f;	//Vitesse actuelle
+	private float accelerationRate = 1.2f;	//Taux d'acceleration
+
+	//Gere chacuns des rayons de detection
+	public float maxDistance = 5f;
+	public float distForward = 35f;
+	public float distLeft = 5f;
+	public float distRight = 5f;
+	public float distDiagLeft = 5f;
+	public float distDiagRight = 5f;
+
+	public float fitness = 0f;			//Gere le fitness
+	private Vector3 lastPosition;
+	private float distanceTraveled;
+
+	public float[] results;				// Resultat du feed forward. Contient deux float, le premier pour l'acceleration, le deuxieme pour l'orientation
+	public bool active = true;			//Passe a False si l'agent heurte un mur
+
+
+	void Start()
+	{
+		lastPosition = transform.position;
+	}
+	// Chaque frame :
+	void Update ()
+	{
+		//Tant que l'agent est en vie
+		if (active) {
+			CharacterController controller = gameObject.GetComponent<CharacterController> ();
+
+			// Si le neural network nous a dit quoi faire... Bah le tichat le fait
+			if (results.Length != 0) {
+
+				currentVelocity += (accelerationRate * Time.deltaTime)*results [0];//Applique le resultat du premier neurone d'output sur la vitesse
+				currentVelocity = Mathf.Clamp (currentVelocity, initialVelocity, finalVelocity);// Clamp la velocite entre la valeur initiale et finale
+
+				movDir = new Vector3 (0, 0, currentVelocity); //Met la velocite actuelle sous forme de vecteur
+
+				movDir *= speed;
+				movDir = transform.TransformDirection (movDir);
+
+				controller.Move (movDir);//Deplace le tichat
+				transform.Rotate (0, results [1] * rotSpeed * Time.deltaTime, 0);//Tourne le tichat
+			}
+			InteractRaycast ();
+		}
+		distanceTraveled += Vector3.Distance(transform.position, lastPosition);
+		lastPosition = transform.position;
+		fitness += distanceTraveled/1000;//Augmente le fitness en fonction de la distance parcourue
+		fitness -= 0.01f; //Decroit le fitness au long du temps
+	}
+
+    // Collisions
+    //Si le tichat touche un mur, PAF il est mort
+    //Par contre si il touche un checkpoint, il gagne en fitness
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Mur"){
+			active = false;
+			fitness/=10;
+		}
+		else if (other.gameObject.tag == "checkpoint"){
+			fitness += 4f;
+		}
+		else if (other.gameObject.tag == "Sortie"){
+			fitness += 50f;
+			active = false;
+		}
+	}
+
+	//Gere les rayons pour detecter les murs
+	void InteractRaycast()
+	{
+
+		transf = GetComponent<Transform>();
+		Vector3 playerPosition = transform.position;
+
+		//Cree les directions de chaque rayons
+		Vector3 forwardDirection = transf.forward*10;
+		Vector3 leftDirection = transf.right * -10;
+		Vector3 rightDirection = transf.right*10;
+		Vector3 diagLeft = transf.TransformDirection (new Vector3 (maxDistance / 5, 0f, maxDistance / 5));
+		Vector3 diagRight = transf.TransformDirection(new Vector3(-maxDistance/5, 0f, maxDistance/5));
+
+		//Cree les rayons
+		Ray myRay = new Ray(playerPosition, forwardDirection);
+		Ray leftRay = new Ray(playerPosition, leftDirection);
+		Ray rightRay = new Ray(playerPosition, rightDirection);
+		Ray diagLeftRay = new Ray(playerPosition, diagLeft);
+		Ray diagRightRay = new Ray(playerPosition, diagRight);
+
+		//Gere les collisions des rayons et recupere la distance si ils rencontrent quelque chose
+		RaycastHit hit;
+		if(Physics.Raycast(myRay, out hit, maxDistance*10)&& hit.transform.tag == "Mur")
+		{
+			distForward = hit.distance;
+		}
+		if(Physics.Raycast(leftRay, out hit, maxDistance*10)&& hit.transform.tag == "Mur")
+		{
+			distLeft = hit.distance;
+		}
+		if(Physics.Raycast(rightRay, out hit, maxDistance*10)&& hit.transform.tag == "Mur")
+		{
+			distRight = hit.distance;
+		}
+		if(Physics.Raycast(diagLeftRay, out hit, maxDistance*10)&& hit.transform.tag == "Mur")
+		{
+			distDiagLeft = hit.distance;
+		}
+		if(Physics.Raycast(diagRightRay, out hit, maxDistance*10)&& hit.transform.tag == "Mur")
+		{
+			distDiagRight = hit.distance;
+		}
+	}
+}
